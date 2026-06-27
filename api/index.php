a12f270cf99a391b9a4d02c02232068727ed8ccf
Jundi Asyura Muzhaffar <if23.jundimuzhaffar@mhs.ubpkarawang.ac.id>
2026-06-27 15:52:30 +0700

    Fix file api index bridge

--- api/index.php
+++ api/index.php
@@ -1,2 +1,18 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+// Memuat file autoload bawaan Laravel
+require __DIR__ . '/../vendor/autoload.php';
+
+// Memuat aplikasi Laravel (bootstrap)
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Menjalankan request lewat Kernel Laravel
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
