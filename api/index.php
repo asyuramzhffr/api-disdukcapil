36f8f175b4bb9f3fddd0db8e7dd4d228d7e1e0e3
Jundi Asyura Muzhaffar <if23.jundimuzhaffar@mhs.ubpkarawang.ac.id>
2026-06-27 16:19:49 +0700

    Merge environment bypass and sequence fixes

--- api/index.php
+++ api/index.php
@@ -1,5 +1,22 @@
 <?php
 
+// Pasang kembali paksaan environment agar Laravel tidak bingung di Vercel
+$appEnv = [
+    'APP_ENV' => 'production',
+    'APP_DEBUG' => 'true', // Biar kalau ada error database/env langsung kelihatan teksnya, bukan abu-abu lagi
+    'LOG_CHANNEL' => 'stderr',
+    'VIEW_COMPILED_PATH' => '/tmp',
+    'CACHE_DRIVER' => 'array',
+    'SESSION_DRIVER' => 'array',
+    'QUEUE_CONNECTION' => 'sync',
+];
+
+foreach ($appEnv as $key => $value) {
+    putenv("$key=$value");
+    $_ENV[$key] = $value;
+    $_SERVER[$key] = $value;
+}
+
 // Memuat file autoload bawaan Laravel
 require __DIR__ . '/../vendor/autoload.php';
 
