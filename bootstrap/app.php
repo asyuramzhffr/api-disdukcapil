21d14cf023d3d7e0806cd32953b7d807534271ae
Jundi Asyura Muzhaffar <if23.jundimuzhaffar@mhs.ubpkarawang.ac.id>
2026-06-27 16:23:50 +0700

    Hard override PackageManifest class

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,25 +1,25 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-*/
-
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
-// --- SEGERA KUNCI JALUR STORAGE DAN MANIFEST DI SINI ---
+// --- POTONG KOMPAS CONFIG& MANIFEST LANGSUNG DI INSTANCE ---
 $app->useStoragePath('/tmp');
-$app->instance('manifest.path', '/tmp/packages.php');
-// --------------------------------------------------------
 
-/*
-|--------------------------------------------------------------------------
-| Bind Important Interfaces
-|--------------------------------------------------------------------------
-*/
+// Kita override path config & cache langsung ke /tmp
+$app->instance('path.config', '/tmp');
+$app->instance('manifest.path', '/tmp/packages.php');
+
+// Trik Pamungkas: Daftarkan ulang PackageManifest khusus untuk Vercel
+$app->singleton(\Illuminate\Foundation\PackageManifest::class, function () use ($app) {
+    return new \Illuminate\Foundation\PackageManifest(
+        new \Illuminate\Filesystem\Filesystem,
+        '/tmp', // Base path diubah ke tmp
+        '/tmp/packages.php' // File path diubah ke tmp
+    );
+});
+// ------------------------------------------------------------
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
@@ -35,11 +35,5 @@ $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
     App\Exceptions\Handler::class
 );
-
-/*
-|--------------------------------------------------------------------------
-| Return The Application
-|--------------------------------------------------------------------------
-*/
 
 return $app;
