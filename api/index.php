ddc542dc89466869f6dc33221fe1d87bb4583be9
Jundi Asyura Muzhaffar <if23.jundimuzhaffar@mhs.ubpkarawang.ac.id>
2026-06-27 16:17:20 +0700

    Fix total package manifest path sequencing

--- api/index.php
+++ api/index.php
@@ -1,30 +1,12 @@
 <?php
 
-// Bypass folder storage & cache agar mengarah ke folder internal Vercel (/tmp)
-$appEnv = [
-    'APP_ENV' => 'production',
-    'APP_DEBUG' => 'true',
-    'LOG_CHANNEL' => 'stderr',
-    'VIEW_COMPILED_PATH' => '/tmp',
-    'CACHE_DRIVER' => 'array',
-    'SESSION_DRIVER' => 'array',
-    'QUEUE_CONNECTION' => 'sync',
-];
-
-foreach ($appEnv as $key => $value) {
-    putenv("$key=$value");
-    $_ENV[$key] = $value;
-    $_SERVER[$key] = $value;
-}
-
-// Jalankan bootstrap Laravel
+// Memuat file autoload bawaan Laravel
 require __DIR__ . '/../vendor/autoload.php';
+
+// Memuat aplikasi Laravel (bootstrap)
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// PAKSA JALUR MANIFEST DI SINI SEBELUM KERNEL BERJALAN
-$app->useStoragePath('/tmp');
-$app->instance('manifest.path', '/tmp/packages.php');
-
+// Menjalankan request lewat Kernel Laravel
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,26 +4,21 @@
 |--------------------------------------------------------------------------
 | Create The Application
 |--------------------------------------------------------------------------
-|
-| The first thing we will do is create a new Laravel application instance
-| which serves as the "glue" for all the components of Laravel, and is
-| the IoC container for the system binding all of the various parts.
-|
 */
 
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+// --- SEGERA KUNCI JALUR STORAGE DAN MANIFEST DI SINI ---
+$app->useStoragePath('/tmp');
+$app->instance('manifest.path', '/tmp/packages.php');
+// --------------------------------------------------------
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
 |--------------------------------------------------------------------------
-|
-| Next, we need to bind some important interfaces into the container so
-| we will be able to resolve them when needed. The kernels serve the
-| incoming requests to this application from both the web and CLI.
-|
 */
 
 $app->singleton(
@@ -45,22 +40,6 @@ $app->singleton(
 |--------------------------------------------------------------------------
 | Return The Application
 |--------------------------------------------------------------------------
-|
-| This script returns the application instance. The instance is given to
-| the calling script so we can separate the building of the instances
-| from the actual running of the application and sending responses.
-|
 */
-
-// Tambahkan baris ini agar Laravel pakai folder temporary Vercel
-$app->useStoragePath('/tmp');
-
-// ... kode Laravel lainnya di atas ...
-
-// Paksa storage path ke folder tmp Vercel
-$app->useStoragePath('/tmp');
-
-// Tambahkan baris ini untuk memindahkan cache manifest package ke tmp biar tidak error writable!
-$app->instance('manifest.path', '/tmp/packages.php');
 
 return $app;
