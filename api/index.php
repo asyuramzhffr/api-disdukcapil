1442f81e76ae2140021644280fc1eea15e3a8bfe
Jundi Asyura Muzhaffar <if23.jundimuzhaffar@mhs.ubpkarawang.ac.id>
2026-06-27 16:03:41 +0700

    Bypass config via api index

--- api/index.php
+++ api/index.php
@@ -1,12 +1,28 @@
 <?php
 
-// Memuat file autoload bawaan Laravel
-require __DIR__ . '/../vendor/autoload.php';
+// Bypass folder storage & cache agar mengarah ke folder internal Vercel (/tmp)
+$appEnv = [
+    'APP_ENV' => 'production',
+    'APP_DEBUG' => 'true', // Kita buat true dulu agar kalau error, teks error aslinya kelihatan di browser!
+    'LOG_CHANNEL' => 'stderr',
+    'VIEW_COMPILED_PATH' => '/tmp',
+    'CACHE_DRIVER' => 'array',
+    'SESSION_DRIVER' => 'array',
+    'QUEUE_CONNECTION' => 'sync',
+];
 
-// Memuat aplikasi Laravel (bootstrap)
+foreach ($appEnv as $key => $value) {
+    putenv("$key=$value");
+    $_ENV[$key] = $value;
+    $_SERVER[$key] = $value;
+}
+
+// Jalankan bootstrap Laravel seperti biasa
+require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Menjalankan request lewat Kernel Laravel
+$app->useStoragePath('/tmp');
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
