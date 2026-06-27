0dd9dbb64bfd59b49fd3ed042459fc63340af2d5
Jundi Asyura Muzhaffar <if23.jundimuzhaffar@mhs.ubpkarawang.ac.id>
2026-06-27 17:02:02 +0700

    Bersih total cache

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,32 +1,12 @@
 <?php
 
-// 1. Tentukan path sementara
-$tmpPath = '/tmp';
-
-// 2. Buat folder bayangan kalau belum ada
-if (!is_dir($tmpPath . '/bootstrap/cache')) {
-    mkdir($tmpPath . '/bootstrap/cache', 0755, true);
-}
-
-// 3. Paksa setting environment PHP sebelum aplikasi dibuat
-putenv("APP_STORAGE=$tmpPath/storage");
-
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
-// 4. Override path menggunakan method yang didukung Laravel
-$app->useStoragePath($tmpPath . '/storage');
-$app->useBootstrapPath($tmpPath . '/bootstrap');
-
-// 5. Daftarkan path manifest secara manual agar tidak error
-$app->instance('manifest.path', $tmpPath . '/bootstrap/cache/packages.php');
-
-/*
-|--------------------------------------------------------------------------
-| Bind Important Interfaces
-|--------------------------------------------------------------------------
-*/
+// Paksa agar Laravel menggunakan folder /tmp untuk semua cache
+$app->useStoragePath('/tmp');
+$app->useBootstrapPath($app->basePath() . '/bootstrap');
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
